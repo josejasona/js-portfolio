4b7276d3f2970bc358f4a894c3f709da382ccf93
josejasona <jose.jason.a@gmail.com>
2023-12-11 15:41:33 -0500

    Added shadcn to project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
-import { Layout } from "~/components/layout";
-
-import { api } from "~/utils/api";
 
 export default function Home() {
-  return <Layout></Layout>;
+  return <div>text</div>;
 }
